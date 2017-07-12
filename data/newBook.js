0330f853301d6165ebd2a70600cfde5cf82ca33d
tongjie <tongjieco@163.com>
2017-07-12 23:01:38 +0800

    add parts 20170712

---
+++ data/newBook.js
@@ -0,0 +1,26 @@
+[
+	{
+		
+		"img":"http://cover.read.duokan.com/mfsv2/download/fdsc3/p01cOgYj9QRW/daQp37uxJl5AiH.jpg!t",
+		"name":"24重人格（十周年纪念版）"
+	},
+	{
+		
+		"img":"http://cover.read.duokan.com/mfsv2/download/fdsc3/p01FyVEtPbiW/ZnvzV8Km16JX9a.jpg!t",
+		"name":"阿尔米汗：我行我素"
+	},
+	{
+		
+		"img":"http://cover.read.duokan.com/mfsv2/download/fdsc3/p01quTWX3Pmj/ZNdAhrl3Hw9itf.jpg!t",
+		"name":"变形金刚3"
+	},
+	{
+		
+		"img":"http://cover.read.duokan.com/mfsv2/download/fdsc3/p01UQQVyvV97/cRe4afo7X4I1sy.jpg!t",
+		"name":"悟空传（典藏纪念版）"
+	}
+]
+
+
+
+
--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-var imgData = [];//存放ppt图片数组
+var newBookImgArr = [];//存放ppt图片数组
 var imgDataArr = [];
 var imgNum = 0;
+var newBookImgNum = 0;
 var pptAjaxObj = null;
 $(function() {
     $(window).load(function() {
@@ -24,18 +25,48 @@ $(function() {
                 }
             }
         });
-    });
- 
+        $.ajax({
+            url: 'data/newBook.js?',
+            type: 'get',
+            data: {
+                // pageidx: 1,
+                // pagenum: 100,
+                // starttime: starttime,
+                // endtime: endtime,
+            },
+            success: function(data) {
+                if(typeof(data) != "undefined" && data.length > 0){
+                    var ajaxData = eval('('+data+')');
+                    newBookImgArr = ajaxData;
+                    // console.log("ajax ajaxData="+ajaxData);
+                    // for (var i = 0; i < ajaxData.length; i++) {
+                    //     newBookImgArr[i] = ajaxData[i].img;
+                    // }
+                    changeNewBookImg();
+                }
+            }
+        });
+    }); 
 });
 
 function changeImg(){
     $('#img_src').attr('src',imgDataArr[imgNum]);
     setInterval(function(){
-        imgNum = (++imgNum)%5;
+        imgNum = (++imgNum)%(imgDataArr.length);
         $('#img_src').attr('src',imgDataArr[imgNum]);
         // console.log(imgDataArr[imgNum])
     },3000); 
 }
 
-
+function changeNewBookImg() {
+    $('#newBook').attr('src',newBookImgArr[newBookImgNum].img);
+    $('#newBookName').text(newBookImgArr[newBookImgNum].name);
+    // console.log(newBookImgArr)
+    setInterval(function () {
+        newBookImgNum = (++newBookImgNum)%(newBookImgArr.length);
+        // console.log(newBookImgArr[newBookImgNum])
+        $('#newBook').attr('src',newBookImgArr[newBookImgNum].img);
+        $('#newBookName').text(newBookImgArr[newBookImgNum].name);
+    },1500);
+}
 
